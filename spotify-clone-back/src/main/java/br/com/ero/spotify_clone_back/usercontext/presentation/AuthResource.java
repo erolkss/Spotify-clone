e71828ee3089d039b45c90773f4de9777f016885
Lucas Lima <lucaserolima@gmail.com>
2025-02-04 15:52:32 -0400

    fix: inject UserService and Okta ClientRegistration into AuthResource constructor"

--- spotify-clone-back/src/main/java/br/com/ero/spotify_clone_back/usercontext/presentation/AuthResource.java
+++ spotify-clone-back/src/main/java/br/com/ero/spotify_clone_back/usercontext/presentation/AuthResource.java
@@ -3,12 +3,12 @@ package br.com.ero.spotify_clone_back.usercontext.presentation;
 import br.com.ero.spotify_clone_back.usercontext.ReadUserDTO;
 import br.com.ero.spotify_clone_back.usercontext.application.UserService;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,12 +20,15 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@RequiredArgsConstructor
 public class AuthResource {
 
     private final UserService userService;
     private final ClientRegistration registration;
 
+    public AuthResource(UserService userService, ClientRegistrationRepository registrations) {
+        this.userService = userService;
+        this.registration = registrations.findByRegistrationId("okta");
+    }
 
     @GetMapping("/get-authenticated-user")
     public ResponseEntity<ReadUserDTO> getAuthenticatedUser(@AuthenticationPrincipal OAuth2User user) {
