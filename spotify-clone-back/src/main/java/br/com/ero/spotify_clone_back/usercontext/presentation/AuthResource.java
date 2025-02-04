cc3dd34c0086ebbb58257153cb80dd7213f0b7eb
Lucas Lima <lucaserolima@gmail.com>
2025-02-04 15:38:16 -0400

    feat: implement logout endpoint to invalidate session and redirect to IdP logout

--- spotify-clone-back/src/main/java/br/com/ero/spotify_clone_back/usercontext/presentation/AuthResource.java
+++ spotify-clone-back/src/main/java/br/com/ero/spotify_clone_back/usercontext/presentation/AuthResource.java
@@ -2,15 +2,21 @@ package br.com.ero.spotify_clone_back.usercontext.presentation;
 
 import br.com.ero.spotify_clone_back.usercontext.ReadUserDTO;
 import br.com.ero.spotify_clone_back.usercontext.application.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.text.MessageFormat;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -31,5 +37,16 @@ public class AuthResource {
             return ResponseEntity.ok().body(userFromAuthentication);
         }
     }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletRequest request) {
+        String issuerUri = registration.getProviderDetails().getIssuerUri();
+        String originUrl = request.getHeader(HttpHeaders.ORIGIN);
+        Object[] params = {issuerUri, registration.getClientId(), originUrl};
+        String logoutUrl = MessageFormat.format("{0}v2/logout?clientId={1}&returnTo={2}", params);
+        request.getSession().invalidate();
+        return ResponseEntity.ok().body(Map.of("logoutUrl", logoutUrl));
+    }
+
 
 }
